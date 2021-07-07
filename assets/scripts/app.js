97eef755edd0f16d6c84c497f5dac68d44f7d493
danielamaaria <daniela.galatan28@gmail.com>
2021-07-07 19:43:45 +0300

    Introducing functions to our app

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) { // curly braces named function body
+    const result = num1 + num2;
+    alert('The result is' + result); // built-in functions
+};
+
+// add(1, 2); //This line executes and calls the function, it runs that function
+
 currentResult = (currentResult + 10) * 3 / 2 - 1 ;
 
-let calculationDescription = '(' + currentResult + '+ 10 * 3 / 2 - 1';
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription); // call a function
