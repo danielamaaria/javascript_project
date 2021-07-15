368cd48ab7cf0b19e32811ebdb7b69dd2e7eaa2e
danielamaaria <daniela.galatan28@gmail.com>
2021-07-15 22:04:24 +0300

    String concatenation beetwen 2 strings

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,8 +3,9 @@ let currentResult = defaultResult;
 
 
 function add() { // curly braces named function body
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+    const calcDescription = `${currentResult} + ${(userInput.value)}`
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult, calcDescription);
     //alert('The result is' + result); // built-in functions
     // return result; //returns the value that's stored in result as a result of this function call
 };
@@ -14,7 +15,7 @@ function add() { // curly braces named function body
 // currentResult = (currentResult + 10) * 3 / 2 - 1 ;
 
 
-addBtn.addEventListener('click', add);
+addBtn.addEventListener('click', add); //Meaning: "Hey, when the button is clicked, go ahead and execute add.".
 
 // let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
