93e7989b647d2a9d63eb2974820cc5a571cc68bc
danielamaaria <daniela.galatan28@gmail.com>
2021-07-18 21:22:22 +0300

    Connecting all buttons to functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,55 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
 
 function add() { // curly braces named function body
-    const calcDescription = `${currentResult} + ${(userInput.value)}`
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, calcDescription);
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initalResult, enteredNumber);
     //alert('The result is' + result); // built-in functions
     // return result; //returns the value that's stored in result as a result of this function call
-};
+}
 
 // currentResult= add(1, 2);
 // //add(5, 5); //This line executes and calls the function, it runs that function
 // currentResult = (currentResult + 10) * 3 / 2 - 1 ;
 
 
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initalResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initalResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initalResult, enteredNumber);
+}
+
 addBtn.addEventListener('click', add); //Meaning: "Hey, when the button is clicked, go ahead and execute add.".
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 // let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-outputResult(currentResult, calculationDescription); // call a function
+// outputResult(currentResult, calculationDescription); // call a function
